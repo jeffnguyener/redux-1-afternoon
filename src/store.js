5a45d4bbfe999fdabb1dd29e54fa6c3e716323cf
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-20 16:20:05 -0600

    update store and instruct

--- src/store.js
+++ src/store.js
@@ -5,7 +5,9 @@ const initialState = {
     category: " ",
     authorFirst: " ",
     authorLast: " ",
-    ingredients: []
+    ingredients: [],
+    instructions: [],
+    recipes: []
 }
 
 export const UPDATE_NAME = 'UPDATE_NAME'
@@ -13,6 +15,8 @@ export const UPDATE_CATEGORY = 'UPDATE_CATEGORY'
 export const UPDATE_AUTHOR_FIRST = 'UPDATE_AUTHOR_FIRST'
 export const UPDATE_AUTHOR_LAST = 'UPDATE_AUTHOR_LAST'
 export const ADD_INGREDIENT = 'ADD_INGREDIENT'
+export const ADD_INSTRUCTION = 'ADD_INSTRUCTION'
+export const ADD_RECIPE = 'ADD_RECIPE'
 
 function reducer(state = initialState, action) {
     const { type, payload } = action
@@ -28,6 +32,14 @@ function reducer(state = initialState, action) {
         case ADD_INGREDIENT:
             const newIngredients = [...state.ingredients, payload]
             return { ...state, ingredients: newIngredients }
+        case ADD_INSTRUCTION:
+            const newInstructions = [...state.instructions, payload]
+            return { ...state, instructions: newInstructions }
+        case ADD_RECIPE:
+            const { name, category, authorFirst, authorLast, ingredients, instructions } = state;
+            const recipe = { name, category, authorFirst, authorLast, ingredients, instructions };
+            const newRecipes = [...state.recipes, recipe]
+            return { ...state, recipes: newRecipes }
         default:
             return state
     }
